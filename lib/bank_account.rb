0fa69f0bed11c7a8cab6fb81b5d7c5fc8be07e83
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 20:51:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  #@@account = []
+def initialize(name)
+  @name = name
+  #@balance = balance
+  @balance = 1000
+  #@status = status
+  @status = "open"
+end
+  def deposit(money)
+    self.balance += money.to_i
+    #can deposit money into its account
+  end
 
+  def display_balance
+    "Your balance is $#{@balance}."
+    #@@account
+     #can display its balance
+  end
+  def valid?
+    balance > 0 && status == "open"
+    #self.status == 'open' && self.balance #? true : false
+    #if ((status = "open") == (@balance > 0))
+    #true
+  #else
+    #false
+     #is valid with an open status and a balance greater than 0
+  #end
+end
+  def close_account
+    #can close its account
+    self.status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :amount, :status, :balance
+#attr_reader :status
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    status = "pending"
+    @status = status
+    @amount = amount
+  end
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  #######################################
+#   def execute_transaction
+#     #if @sender.balance > amount
+#       if @sender.balance > @amount
+#         sender.balance -= amount
+#         receiver.balance += amount
+#         self.status = "complete"
+#       else
+#       self.status = 'rejected'
+#     puts  "Transaction rejected. Please check your account balance."
+#       end
+# end
+def execute_transaction
+  if @sender.balance < @amount
+     @status = "rejected"
+     puts "Transaction rejected. Please check your account balance."
+  #   return "Transaction rejected. Please check your account balance."
+
+  elsif @status == "complete"
+        puts "Transaction was already executed"
+  else
+    @sender.deposit( @amount * -1 )
+    @receiver.deposit( @amount )
+    @status = "complete"
+    #puts "Transaction was already executed"
+  end
 end
+
+  def reverse_transfer
+  #if valid? && receiver.balance > amount && self.status == "complete"
+      if  @status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status == "reversed"
+      else
+      @status == "rejected"
+    end
+   end
+ end
